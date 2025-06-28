9a7ed66e48066c28114bcc1899af70dd3ab0b6fa
ajiekusumadhany <ajieikruskusumadhany@gmail.com>
2025-06-28 12:24:36 +0700

    upload first

--- themes/hugo-theme-luna/postcss.config.js
+++ themes/hugo-theme-luna/postcss.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const isDev = process.env.HUGO_ENVIRONMENT !== 'production';
+
 module.exports = {
     modules: true,
     plugins: !isDev ? [
-        require('tailwindcss')({
+        require('@tailwindcss/postcss')({
             config: path.join(__dirname, '/tailwind.config.js')
         }),
         require('postcss-pxtorem')({
@@ -17,7 +18,7 @@ module.exports = {
             display: 'swap',
             replace: false
         }),
-        require('@fullhuman/postcss-purgecss')({
+        require('@fullhuman/postcss-purgecss').default({
             content: [
                 path.join(__dirname, '/data/luna/icon.yaml').split(path.sep).join(path.posix.sep),
                 path.join(__dirname, '/layouts/**/*.html').split(path.sep).join(path.posix.sep),
@@ -25,11 +26,8 @@ module.exports = {
                 path.join(__dirname, '/assets/**/*.ts').split(path.sep).join(path.posix.sep),
             ],
             safelist: {
-                // xx
                 standard: [/scrollbar/, /sm|md|lg|xl|2xl|[0-9]{1,2}\.[0-9]{1,2}/, /:/, /\//, /h[1-6]|hover|dark|last|after|before/, 'a', 'blockquote', 'body', 'code', 'fieldset', 'figure', 'hr', 'html', 'img', 'kbd', 'ol', 'p', 'pre', 'strong', 'sup', 'table', 'ul'],
-                // xx包括子项
                 deep: [/skiptranslate/, /goog-te/, /katex/, /hugo-encrypt/, /medium-zoom/, /lazy/, /swup/, /eva/],
-                // button.bg-xx.other-class
                 greedy: []
             },
             keyframes: true,
@@ -38,14 +36,11 @@ module.exports = {
             rejected: true,
         }),
         require('cssnano')({
-            preset: ['cssnano-preset-advanced', {
-                discardComments: {
-                    removeAll: true,
-                }
-            }]
-        }),
+  preset: 'default',
+}),
+
     ] : [
-        require('tailwindcss')({
+        require('@tailwindcss/postcss')({
             config: path.join(__dirname, '/tailwind.config.js')
         }),
         require('postcss-easing-gradients')({}),
